5916b7699928412b0c6f534c34c14a3fe3ea9d42
VinczeDaniel2 <vindani919@gmail.com>
2024-11-13 15:09:38 +0100

    asd

--- configDB.js
+++ configDB.js
@@ -1,8 +1,20 @@
-export const configDB={
+import dotenv from 'dotenv'
+dotenv.config() 
+
+export const configDB_={
     host:'localhost',
     user:'root',
     password:'',
     database:'todos',
     port:3307,
     multipleStatements:true//engedélyezünk több utsítást
+}
+
+export const configDB={
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE,
+    port:process.env.DB_PORT,
+    multiplieStatement:true
 }
